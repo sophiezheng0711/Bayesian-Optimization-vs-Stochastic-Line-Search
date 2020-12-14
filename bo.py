26132ed3913eb28333e589376ee2fe73ef7c6ce1
sz374 <sz374@cornell.edu>
2020-12-14 07:26:25 -0500

    added mushrooms dataset for bo

--- bo.py
+++ bo.py
@@ -2,20 +2,17 @@ import numpy as np
 from matplotlib import pyplot as plt
 from warnings import catch_warnings, simplefilter
 from sklearn.model_selection import cross_val_score
-from skopt.space import Integer
 from skopt.utils import use_named_args
 from skopt import gp_minimize
 
-from sklearn.datasets import make_blobs
-from sklearn.neighbors import KNeighborsClassifier
-
 
 class BayesianOptimization():
-    def __init__(self, model, search_space, X, y):
+    def __init__(self, model, search_space, X, y, epochs):
         self.model = model
         self.search_space = search_space
         self.X = X
         self.y = y
+        self.epochs = epochs
 
     def eval(self):
         @use_named_args(self.search_space)
@@ -32,26 +29,10 @@ class BayesianOptimization():
     def run(self):
         with catch_warnings():
             simplefilter('ignore')
-            result = gp_minimize(self.eval(), self.search_space, acq_func='EI', n_calls=10)
-            # to be changed, since this print only works for the example test
+            result = gp_minimize(self.eval(), self.search_space, acq_func='EI', n_calls=self.epochs)
             result_etas = np.array([x[0] for x in result.x_iters])
             result_accs = np.array([1 - x for x in result.func_vals])
             print('Best Accuracy: %.3f' % (1.0 - result.fun))
             print('Best Parameters: eta0=%.5e' % (result.x[0]))
-            print(result_etas)
-            print(result_accs)
             plt.scatter(result_etas, result_accs)
-            plt.legend()
             plt.show()
-        
-
-if __name__ == "__main__":
-    # make test data. To be replaced with real data.
-    X, y = make_blobs(n_samples=500, centers=3, n_features=2)
-    model = KNeighborsClassifier()
-    # define search space. In this example, there are two hyperparameters we are trying to optimize.
-    # in addition, this is an integer space. We could also use skopt.space.space.Real, for real parameters.
-    search_space = [Integer(1, 5, name='n_neighbors'), Integer(1, 2, name='p')]
-
-    bo = BayesianOptimization(model, search_space, X, y)
-    bo.run()
--- main.py
+++ main.py
@@ -1,18 +1,40 @@
-# from sklearn.linear_model import SGDClassifier
+from sklearn.linear_model import SGDClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.datasets import fetch_openml
 from skopt.space import Real
+import numpy as np
+import argparse
 
 from bo import BayesianOptimization
 
 if __name__ == "__main__":
-    mnist = fetch_openml('mnist_784', version=1)
-    X, y = mnist['data'], mnist['target']
-    X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '-dataset', required=True)
+    args = parser.parse_args()
 
-    # model = SGDClassifier(loss='log', random_state=42, learning_rate='constant', eta0=0.1)
-    model = MLPClassifier(hidden_layer_sizes=(512, 256), activation='logistic', solver='sgd', batch_size=128, learning_rate='constant', learning_rate_init=0.1)
-    search_space = [Real(1e-7, 0.1, name='learning_rate_init')]
+    epochs = 10
 
-    bo = BayesianOptimization(model, search_space, X_train, y_train)
-    bo.run()
+    if args.d is not None:
+        if args.d == 'mushroom':
+            mnist = fetch_openml('mushroom', version=1)
+            X, y = mnist['data'], mnist['target']
+            X = np.nan_to_num(X)
+            y = np.nan_to_num(y)
+
+            model = SGDClassifier(loss='log', random_state=42, learning_rate='constant', eta0=0.1)
+            search_space = [Real(1e-7, 0.1, name='eta0')]
+
+            bo = BayesianOptimization(model, search_space, X, y, epochs)
+            bo.run()
+        elif args.d == 'mnist':
+            mnist = fetch_openml('mnist_784', version=1)
+            X, y = mnist['data'], mnist['target']
+            X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+
+            model = MLPClassifier(hidden_layer_sizes=(512, 256), activation='logistic', solver='sgd', batch_size=128, learning_rate='constant', learning_rate_init=0.1)
+            search_space = [Real(1e-7, 0.1, name='learning_rate_init')]
+
+            bo = BayesianOptimization(model, search_space, X, y, epochs)
+            bo.run()
+        else:
+            raise ValueError("invalid datasets")
